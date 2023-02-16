aba12e232e31410b11bf7108ff180397be1ebb07
nyanshiba <31783332+nyanshiba@users.noreply.github.com>
2023-02-16 21:10:13 +0900

    configの改行コードに対応
    長いconfigに対応するため、購読時間を長くした

--- get-running-config.sh
+++ get-running-config.sh
@@ -1,14 +1,14 @@
 # オペレーションモードからグローバルコンフィグモードへの移行
-echo 'exit' > /dev/ttyS3
-sleep 5
 echo 'configure' > /dev/ttyS3
 echo 'terminal length 0' > /dev/ttyS3
-sleep 5
+echo 'event-terminal stop' > /dev/ttyS3
+sleep 3
 
 # 10秒間COM3をファイルに書き出す
-# nohup sh -c 'timeout 60 cat -e /dev/ttyS3' > running-config.cfg &
-nohup sh -c 'timeout 60 cat /dev/ttyS3' | tr -d '\r' > running-config.cfg 2>&1 &
+nohup sh -c 'timeout 300 cat /dev/ttyS3' | tr -s '\r\n' > running-config.cfg 2>&1 &
+echo 'listening...'
 sleep 5
 
 # 現在の設定情報を表示するコマンドを送信
 echo 'show running-config' > /dev/ttyS3
+sleep 300
